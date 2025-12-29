8247ea9659d3870980e987edece55830ecd7aaf4
Bharathganesh45511 <72161378+Bharathganesh45511@users.noreply.github.com>
2025-12-29 17:25:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 
@@ -66,15 +65,8 @@ y = df[target]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-model = RandomForestRegressor(
-    n_estimators=300,
-    max_depth=None,
-    random_state=42
-)
-
+model = LinearRegression() model.fit(X_train, y_train)
 model.fit(X_train, y_train)
-
-
 # -----------------------------
 # MODEL PERFORMANCE
 # -----------------------------
@@ -148,6 +140,7 @@ if st.button("Predict Concentrate SiO₂"):
 
     prediction = model.predict(input_df)[0]
     st.success(f"Predicted Concentrate SiO₂: {prediction:.2f} %")
+
 
 
 
