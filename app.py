31098f20e16d669de33e1c0fdf483143eb790783
Bharathganesh45511 <72161378+Bharathganesh45511@users.noreply.github.com>
2025-12-29 17:27:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ y = df[target]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-model = LinearRegression() model.fit(X_train, y_train)
+model = LinearRegression()
 model.fit(X_train, y_train)
 # -----------------------------
 # MODEL PERFORMANCE
@@ -140,6 +140,7 @@ if st.button("Predict Concentrate SiO₂"):
 
     prediction = model.predict(input_df)[0]
     st.success(f"Predicted Concentrate SiO₂: {prediction:.2f} %")
+
 
 
 
