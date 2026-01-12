cf38e57b56796caf6ab61cb85da79dc02a9a892d
Bharathganesh45511 <72161378+Bharathganesh45511@users.noreply.github.com>
2026-01-12 16:08:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.title("OBP Concentrate Silica Prediction App")
 @st.cache
 def load_data():
     df = pd.read_excel(
-    "OBP_Silica_Cleaned_Data.xlsx",
+    "OBP_Silica_Cleaned_Data.csv",
     sheet_name="Sheet1"
 )
     return df
@@ -119,3 +119,4 @@ if st.button("Predict Concentrate SiO₂"):
 
     prediction = model.predict(input_df)[0]
     st.success(f"Predicted Concentrate SiO₂: {prediction:.2f} %")
+
